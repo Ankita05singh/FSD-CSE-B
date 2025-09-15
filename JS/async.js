ccf9228902499668e854aaeff0b12fd85676c954
Ankita Singh <your_github_email@example.com>
2025-09-15 14:32:28 +0530

    new

--- JS/async.js
+++ JS/async.js
@@ -1,30 +1,72 @@
-function register(){
-    waitfortwosec();
-    console.log("Register end");
+// function register(){
+//     waitfortwosec();
+//     console.log("Register end");
+// }
+// function sendEmail(){
+//     waitfortwosec();
+//     console.log("Email send end");
+// }
+// function login(){
+//     waitfortwosec();
+//     console.log("login end");
+// }
+// function getData(){
+//     waitfortwosec();
+//     console.log("Data received");
+// }
+// function displayData(){
+//     waitfortwosec();
+//     console.log("Data is diplayed");
+// }
+// function waitfortwosec(){
+//     const ms=new Date().getTime()+2000;
+//     while(new Date().getTime()<ms);
+// }
+
+// register();
+// sendEmail();
+// login();
+// getData;
+// displayData();
+
+function register(Cb){
+    setTimeout(()=>{
+        console.log("Register end");
+        Cb();
+    },2000)
 }
-function sendEmail(){
-    waitfortwosec();
-    console.log("Email send end");
+function sendEmail(Cb){
+    setTimeout(()=>{
+        console.log("Email send end");
+        Cb();
+    },2000)
 }
-function login(){
-    waitfortwosec();
-    console.log("login end");
+function login(Cb){
+    setTimeout(()=>{
+        console.log("login end");
+        Cb();
+    },2000)
 }
-function getData(){
-    waitfortwosec();
-    console.log("Data received");
+function getData(Cb){
+    setTimeout(()=>{
+        console.log("Data Received");
+        Cb();
+    },2000)
 }
-function displayData(){
-    waitfortwosec();
-    console.log("Data is diplayed");
-}
-function waitfortwosec(){
-    const ms=new Date().getTime()+2000;
-    while(new Date().getTime()<ms);
+function displayData(Cb){
+    setTimeout(()=>{
+        console.log("Data Displayed");
+        Cb();
+    },2000)
 }
 
-register();
-sendEmail();
-login();
-getData;
-displayData();
+//Callback Hell
+register(()=>{
+    sendEmail(()=>{
+        login(()=>{
+            getData(()=>{
+                displayData();
+            });
+        });
+    });
+});
